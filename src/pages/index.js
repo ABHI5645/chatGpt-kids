966677c74c86b1cc5621f098b1f4829798da9448
Anish <cybertronian217@gmail.com>
2023-09-03 20:36:41 +0530

    add dashboard icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,11 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { LuLayoutDashboard, LuVideo } from "react-icons/lu";
+import { BsChatLeft, BsCode } from "react-icons/bs";
+import { GoImage } from "react-icons/go";
+import { PiMusicNotesSimpleBold } from "react-icons/pi";
+import { FiSettings } from "react-icons/fi";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +19,15 @@ export default function Home() {
           <MenuItem>
             <h2 className="text-2xl font-bold">genai</h2>
           </MenuItem>
-          <MenuItem>Dashboard</MenuItem>
-          <MenuItem>Conversation</MenuItem>
-          <MenuItem>Image Generation</MenuItem>
-          <MenuItem>Video Generation</MenuItem>
-          <MenuItem>Music Generation</MenuItem>
-          <MenuItem>Code Generation</MenuItem>
-          <MenuItem>Settings</MenuItem>
+          <MenuItem icon={<LuLayoutDashboard />}>Dashboard</MenuItem>
+          <MenuItem icon={<BsChatLeft />}>Conversation</MenuItem>
+          <MenuItem icon={<GoImage />}>Image Generation</MenuItem>
+          <MenuItem icon={<LuVideo />}>Video Generation</MenuItem>
+          <MenuItem icon={<PiMusicNotesSimpleBold />}>
+            Music Generation
+          </MenuItem>
+          <MenuItem icon={<BsCode />}>Code Generation</MenuItem>
+          <MenuItem icon={<FiSettings />}>Settings</MenuItem>
         </Menu>
       </Sidebar>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
