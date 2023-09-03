c594eaf1e323be1d6612c1402e093d5426741e3e
Anish <cybertronian217@gmail.com>
2023-09-03 20:27:38 +0530

    add react-pro-sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,28 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex">
+      <Sidebar className="w-1/5 h-screen">
+        <Menu>
+          <MenuItem>
+            <h2 className="text-2xl font-bold">genai</h2>
+          </MenuItem>
+          <MenuItem>Dashboard</MenuItem>
+          <MenuItem>Conversation</MenuItem>
+          <MenuItem>Image Generation</MenuItem>
+          <MenuItem>Video Generation</MenuItem>
+          <MenuItem>Music Generation</MenuItem>
+          <MenuItem>Code Generation</MenuItem>
+          <MenuItem>Settings</MenuItem>
+        </Menu>
+      </Sidebar>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
