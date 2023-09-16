4a21366f28c155cfa984c161c55b057157f55b3e
Anish <cybertronian217@gmail.com>
2023-09-17 01:13:31 +0530

    add huggingface api call

--- src/components/InputWithButton.jsx
+++ src/components/InputWithButton.jsx
@@ -1,11 +1,40 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export function InputWithButton() {
+  const [text, setText] = useState("");
+  const handleClick = () => {
+    console.log(text);
+    async function query(data) {
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/suno/bark-small",
+        {
+          headers: {
+            Authorization: "Bearer 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",
+          },
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await response.json();
+      return result;
+    }
+
+    query({ inputs: text }).then((response) => {
+      console.log(JSON.stringify(response));
+    });
+  };
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="email" placeholder="Enter Text" />
-      <Button type="submit">Convert to audio</Button>
+      <Input
+        type="email"
+        placeholder="Enter Text"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <Button type="submit" onClick={() => handleClick()}>
+        Convert to audio
+      </Button>
     </div>
   );
 }
